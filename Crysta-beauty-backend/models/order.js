cc736abbaabbed3f28ea705aa6e100f984144f61
Savindi Hewage <hewagesavindi@gmail.com>
2025-08-14 12:02:53 +0530

    Create order.js

---
+++ Crysta-beauty-backend/models/order.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    orderId :{
+        type:String,
+        required:true,
+        unique:true
+    },
+    date:{
+        type:Date,
+        required:true,
+        default:Date.now
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:String,
+        required:true,
+        default:"pending"
+    },
+    phoneNumber:{
+        type:String,
+        required:true
+    },
+    billItems:{
+        type:[
+            {
+                productId:String,
+                productName:String,
+                image:String,
+                quantity:Number,
+                price:Number
+            }
+        ],
+        required:true
+
+    }
+   
+});
