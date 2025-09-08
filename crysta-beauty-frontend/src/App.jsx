eff8159a92c9af0d24e1f4c300f1351b21fa68a9
uvinduuvidura <hewagesavindi@gmail.com>
2025-09-08 22:28:04 +0530

    updated using tailwind css

--- crysta-beauty-frontend/src/App.jsx
+++ crysta-beauty-frontend/src/App.jsx
@@ -7,13 +7,18 @@ function App() {
   
 
   return (
-    <div>
-     <Header/>
-     <ProductCard  name="iphone 16" description="aaasssrrtt ddd ttt ddddd" price="1000"/>
-     <ProductCard  name="iphone 15 pro max" description="aaasssrrtt ddd ttt ddddd" price="2000"/>
+    <div className='w-full h-screen bg-pink-200'>
+    <div className="w-[500px] flex flex-col justify-center items-center h-[500px] bg-gray-200 relative">
+      <div className='w-[90px] h-[90px] bg-red-500'></div>
+     <div className='w-[90px] h-[90px] bg-yellow-500 absolute right-[50px] bottom-[50px] z-50'></div>
+     <div className='w-[90px] h-[90px] bg-blue-500 fixed right-[50px] bottom-[50px]'></div>
+     <div className='w-[90px] h-[90px] bg-green-500'></div>
 
     </div>
-  )
+    </div>
+  );
 }
+
+
 
 export default App
--- crysta-beauty-frontend/src/components/header.jsx
+++ crysta-beauty-frontend/src/components/header.jsx
@@ -1,9 +1,11 @@
-import "./header.css"
-export default function Header(){
-    return(
-        <header>
-            <h1>My Store</h1>
-            
-        </header>
-    )
+export default function Header() {
+  return (
+    <header className="w-[50%] bg-gradient-to-r from-blue-500 to-blue-700 shadow-md">
+      <div className="flex items-center justify-center h-[60px]">
+        <h1 className="text-2xl font-bold tracking-wide text-white drop-shadow-md">
+          My Store
+        </h1>
+      </div>
+    </header>
+  );
 }
