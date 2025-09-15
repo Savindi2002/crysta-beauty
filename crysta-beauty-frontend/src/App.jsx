0e4794d8a88d2fff461ab2a1d8bbb8085d838a24
uvinduuvidura <hewagesavindi@gmail.com>
2025-09-15 13:48:13 +0530

    updated admin page

--- crysta-beauty-frontend/src/App.jsx
+++ crysta-beauty-frontend/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import './App.css'
-import ProductCard from './components/product-card'
-import Header from './components/header'
 import LoginPage from './pages/loginPage';
 import AdminPage from './pages/AdminPage';
-import { BrowserRouter } from 'react-router-dom';
+
 
 function App() {
   
@@ -12,8 +10,10 @@ function App() {
   return (
   <BrowserRouter>
       <Routes path="/">
-      <Route path="/admin" element={<AdminPage/>}/>
+      <Route path="/admin/*" element={<AdminPage/>}/>
       <Route path="/login" element={<LoginPage/>}/>
+      <Route path="/" element={<h1>Home</h1>}/>
+      <Route path="/" element={<h1>404 Not Found</h1>}/>
       
       
       </Routes>
--- crysta-beauty-frontend/src/pages/adminPage.jsx
+++ crysta-beauty-frontend/src/pages/adminPage.jsx
@@ -1,8 +1,22 @@
+import { Link, Route, Routes } from "react-router-dom";
+import { FaUsers } from 'react-icons/fa';
+
 export default  function AdminPage(){
     return(
         <div className="w-full h-screen bg-gray-200 flex p-2">
-            <div className="h-full w-[300px] bg-red-900"></div>
-            <div className="h-full bg-white w-[calc(100vw-300px)] rounded-lg"></div>
+            <div className="h-full w-[300px] ">
+           <Link to="/admin/users" className="p-2 border flex  items-center"><FaUsers/>Users</Link>
+           <Link to="/admin/products" className="block p-2">Products</Link>
+           <Link to="/admin/orders" className="block p-2">Orders</Link>
+           </div>
+            <div className="h-full bg-white w-[calc(100vw-300px)] rounded-lg">
+                <Routes path="/*">
+                <Route  path="/users" element={<h1>Users</h1>}/>
+                <Route  path="/products" element={<h1>Products</h1>}/>
+                <Route  path="/Orders" element={<h1>Orders</h1>}/>
+                </Routes>
+            </div>
+            
 
 
 
