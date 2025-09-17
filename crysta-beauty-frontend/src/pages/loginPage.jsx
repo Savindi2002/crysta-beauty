37609164f1ff8f0b59a93e23c2bf022ff9f34746
uvinduuvidura <hewagesavindi@gmail.com>
2025-09-17 14:06:55 +0530

    updated loginPage

--- crysta-beauty-frontend/src/pages/loginPage.jsx
+++ crysta-beauty-frontend/src/pages/loginPage.jsx
@@ -1,9 +1,16 @@
-import {useState} from "react"
+import {useState} from "react";
+import axios from "axios";
+
+
 export default function LoginPage(){
     const[email,setEmail]=useState("")
     const[password,setPassword]=useState("")
 
     function handlelogin(){
+        console.log("Email: ",email)
+        console.log("Password: ",password)
+
+        axios.post("http://localhost:5000/")
         console.log("Login button clicked")
     }
     
@@ -13,12 +20,27 @@ export default function LoginPage(){
         <div className="w-full h-screen bg-[url(/loginpage.jpg)] bg-cover bg-center flex">
             <div className="w-[50%] h-full"></div>
             
-            <div className="w-[50%]h-full flex justify-center items-center "></div>
-            <div className="w-[450px] h-[600px] backdrop-blur-xl shadow-xl rounded-xl"></div>
-            <input className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[5px]" type="email" placeholder="email"/>
-                  <input className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[5px]" type="passowrd" placeholder="password"/>
-                  <button  onClick={handlelogin} className="w-[400px] h-[50px] bg-green-500 text-white rounded-xl cursor-pointer">Login</button>
+            <div className="w-[50%]h-full flex justify-center items-center ">
+           <div className="w-[450px] h-[600px] backdrop-blur-xl shadow-xl rounded-xl">
+
+         
+                 <input onChange={
+                    (e)=>{
+                        setEmail(e.target.value)
+                    }
+                 }
+
+                 className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[5px]" type="email" placeholder="email"/>
+                  <input 
+                  onChange={(
+                    (e)=>{
+                          setPassword(e.target.value)
+                    }
+                  )}className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[5px]" type="passowrd" placeholder="password"/>
+                  <button onClick={handlelogin} className="w-[400px] h-[50px] bg-green-500 text-white rounded-xl cursor-pointer">Login</button>
                   
+                </div>
+                </div>  
         </div>
     )
 
