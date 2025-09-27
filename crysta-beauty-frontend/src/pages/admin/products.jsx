1ad3fb0972633c5b90c37fb09eaafb8a4cf28ad2
uvinduuvidura <hewagesavindi@gmail.com>
2025-09-27 18:29:44 +0530

    updated product.jsx

--- crysta-beauty-frontend/src/pages/admin/products.jsx
+++ crysta-beauty-frontend/src/pages/admin/products.jsx
@@ -26,13 +26,55 @@ export default function AdminProductsPage(){
 
     return(
         <div className="w-full h-full  rounded-lg">
+            <table className="w-full bg-red-900" >
+                <thead>
+                    <tr>
+                        <th className="p-2">Product ID</th>
+                        <th className="p-2">Name</th>
+                        <th className="p-2">Price</th>
+                        <th className="p-2">Labeled Price</th>
+                        <th className="p-2">Stock</th>
+
+                    </tr>
+
+                </thead>
+                <tbody>
+                    <tr>
+                       <td className="p-2">1</td> 
+                       <td className="p-2">Product 1</td> 
+                       <td className="p-2">100</td> 
+                       <td className="p-2">150</td> 
+                       <td className="p-2">10</td> 
+                    </tr>
+                    <tr>
+                       <td className="p-2">1</td> 
+                       <td className="p-2">Product 1</td> 
+                       <td className="p-2">100</td> 
+                       <td className="p-2">150</td> 
+                       <td className="p-2">10</td> 
+                    </tr>
+
+                </tbody>
+         
+            </table>
            {
              products.map(
-                (product)=>{
+                (product,index)=>{
                     console.log("mapping"+product.productId)
-                    return(
-                        <h1>{product.productId}</h1>
-                    )
+                    // return(
+                    //     <div key={index} className="w-full h-[100px] bg-gray-300 flex items-center justify-between p-2">
+                    //         <div className="flex items-center">
+                    //             <img className="w-[50px] h-[50px] rounded-lg" src={product.image} alt=""/>
+                    //             <h1 className="text-xl font-bold ml-2">{product.name}</h1>
+                    //         </div>
+                    //         <div className="flex items-center">
+                                
+                    //             <h1 className="text-xl font-bold ml-2">{product.name}</h1>
+                    //             <button className="bg-red-500 text-white p-2 rounded-lg ml-2">Delete</button>
+                    //         </div>
+                    //         </div>
+                        
+                    // )
                 }
              )
            }
