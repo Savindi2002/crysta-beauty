1ae77db3fb08c274cec20ae754edfa7d7ce86ea5
uvinduuvidura <hewagesavindi@gmail.com>
2025-09-28 09:06:50 +0530

    update addproduct.jsx

--- crysta-beauty-frontend/src/pages/admin/addProductPage.jsx
+++ crysta-beauty-frontend/src/pages/admin/addProductPage.jsx
@@ -1,7 +1,23 @@
+/*
+ productId ,
+    name,
+    altNames ,
+    price,
+    labeledPrice,
+    description,
+    images,
+    stock,
+
+
+*/
+
+
 export default function AddProductForm(){
     return(
         <div className="w-ful h-full rounded-lg bg-red-900">
-            add product form
+            <div className="w-[500px] h-[600px] bg-white">
+
+            </div>
         </div>
     )
 
