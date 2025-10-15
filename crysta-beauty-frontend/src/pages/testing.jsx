16bc5e3178a7c47c9c037d6a5b65c2e027b8991a
uvinduuvidura <hewagesavindi@gmail.com>
2025-10-15 21:54:26 +0530

    testing.jsx

--- crysta-beauty-frontend/src/pages/testing.jsx
+++ crysta-beauty-frontend/src/pages/testing.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 
+
+//https://ucfjibzzteisykfjqnys.supabase.co
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjZmppYnp6dGVpc3lrZmpxbnlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxOTY2MTgsImV4cCI6MjA3NDc3MjYxOH0.WcCHvfSdh0ssNCdyvQrCu9pLiFyT9ikZDUIn7QBosX4
 export default function Testing(){
     const [file,setfile]=useState(null)
+    const supabase=createClient()
 
     function handleUpload(){
+        const[file,setFile]=useState(null)
+        const supabase=createClient("https://ucfjibzzteisykfjqnys.supabase.co",)
+
+        function handleUpload(){
+            supabase.storage.form("images")
+        }
 
     }
   
